33adb1bcc0e673e4e36bd53c52afb9f116e82982
Harshith Raj <harshithraj@Harshiths-MacBook-Pro.local>
2019-09-29 22:31:36 +0530

    earch operation

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import React, { useState, useEffect } from "react";
 import CardList from "./Components/CardList";
 
 import "./App.css";
+import { fileURLToPath } from "url";
 
 const App = () => {
   const [monsters, setMonsters] = useState([]);
+  const [searchField, setSearchFiled] = useState("");
 
   async function fetchUrl(url) {
     const response = await fetch(url);
@@ -16,9 +18,16 @@ const App = () => {
     fetchUrl("https://jsonplaceholder.typicode.com/users");
   }, []);
 
+  const filteredMonsters= monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
+
   return (
     <div className="App">
-      <CardList monsters={monsters} />
+      <input
+        type="search"
+        placeholder="search monsters"
+        onChange={e => setSearchFiled(e.target.value)}
+      />
+      <CardList monsters={filteredMonsters} />
     </div>
   );
 };
